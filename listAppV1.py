5f9435890121b361f55940ce0c14130e7b10c373
conservativeghosts <77815037+conservativeghosts@users.noreply.github.com>
2021-03-09 12:05:18 -0700

    Add files via upload

---
+++ listAppV1.py
@@ -0,0 +1,24 @@
+""""
+Program Goals:
+1. Get the input from the user!
+2. Convert that input to an INT
+3. Add that input to a list
+4. Pull values from a specific intex positions
+""""
+
+def mainProgram():
+    listyBoi = []
+    print("yo, we're gonna work with some lists now")
+    print("Choose one of the following options. Type a number ONLY!")
+    choice = input("1. Add to list, 2. Return a value at an indec position    ")
+    if choice == "1":
+        addToList()
+    elif choice == "2":
+        indexValues()
+
+def addToList():
+
+def indexValues():
+
+if __name__=="__main__":
+    mainProgram()
